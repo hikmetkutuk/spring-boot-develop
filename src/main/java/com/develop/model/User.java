c09f7568fa1c2d41ad23cd0125366fb8263747c4
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-04-17 16:23:35 +0300

    add user model and integrate postgre with docker compose

---
+++ src/main/java/com/develop/model/User.java
@@ -0,0 +1,23 @@
+package com.develop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDateTime birthDetails;
+}
