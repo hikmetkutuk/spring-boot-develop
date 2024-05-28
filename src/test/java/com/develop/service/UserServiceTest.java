407b1a4853deb51181a96a2f3b60f3288caf3a36
HIKMET KUTUK <hikmetkutuk@gmail.com>
2024-05-28 15:53:25 +0300

    login user unit test

--- src/test/java/com/develop/service/UserServiceTest.java
+++ src/test/java/com/develop/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.develop.service;
 
+import com.develop.dto.AuthRequest;
 import com.develop.dto.AuthResponse;
 import com.develop.dto.UserRequest;
+import com.develop.exception.UserLoginException;
 import com.develop.exception.UserRegistrationException;
 import com.develop.model.Role;
 import com.develop.model.User;
@@ -12,7 +14,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -31,16 +37,22 @@ class UserServiceTest {
     private JwtService jwtService;
     @Mock
     private AuthService authService;
+    @Mock
+    private AuthenticationManager authenticationManager;
     @InjectMocks
     private UserService userService;
     private UserRequest userRequest;
+    private AuthRequest authRequest;
     private User user;
+    private AutoCloseable closeable;
+
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        closeable = MockitoAnnotations.openMocks(this);
 
         userRequest = new UserRequest("John Doe", "john@example.com", "password123", Role.USER);
+        authRequest = new AuthRequest("john@example.com", "password123");
         user = User.builder()
                 .name(userRequest.name())
                 .email(userRequest.email())
@@ -80,9 +92,46 @@ class UserServiceTest {
         verify(userRepository).save(any(User.class));
     }
 
+    @Test
+    void login_SuccessfulLogin_ReturnsAuthResponse() {
+        String jwtToken = "mockJwtToken";
+        String refreshToken = "mockRefreshToken";
+
+        when(userRepository.findByEmail(authRequest.email())).thenReturn(Optional.of(user));
+        when(jwtService.generateToken(user)).thenReturn(jwtToken);
+        when(jwtService.generateRefreshToken(user)).thenReturn(refreshToken);
+
+        AuthResponse response = userService.login(authRequest);
+
+        assertEquals(jwtToken, response.accessToken());
+        assertEquals(refreshToken, response.refreshToken());
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(authService).revokeAllUserTokens(user);
+        verify(authService).saveUserToken(user, jwtToken);
+    }
+
+    @Test
+    void login_UserNotFound_ThrowsUserLoginException() {
+        when(userRepository.findByEmail(authRequest.email())).thenReturn(Optional.empty());
+
+        UserLoginException exception = assertThrows(UserLoginException.class, () -> userService.login(authRequest));
+
+        assertEquals("Failed to login user: User not found with email: " + authRequest.email(), exception.getMessage());
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+    }
+
+    @Test
+    void login_AuthenticationFails_ThrowsUserLoginException() {
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(new RuntimeException("Authentication failed"));
+
+        UserLoginException exception = assertThrows(UserLoginException.class, () -> userService.login(authRequest));
+
+        assertEquals("Failed to login user: Authentication failed", exception.getMessage());
+    }
+
     @AfterEach
-    void tearDown() {
-        userRepository = null;
-        userService = null;
+    void tearDown() throws Exception {
+        closeable.close();
     }
 }
